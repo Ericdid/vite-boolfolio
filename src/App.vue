fe7eda28510586d7edb452c589b76b4d22af598a
Ericdid <ericdidom@gmail.com>
2023-11-08 04:31:33 +0100

    add.ProjectCard

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <script>
 import axios from "axios";
+import ProjectCard from "./components/ProjectCard.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      projects: [],
+
+      api: {
+        baseUrl: "http://127.0.0.1:8000/api/",
+      },
+    };
+  },
+
+  components: { ProjectCard },
+
+  methods: {
+    fetchprojects(url = this.api.baseUrl + "projects") {
+      axios.get(url).then((response) => {
+        this.projects = response.data.projects;
+      });
+    },
   },
 
   created() {
-    // console.log(axios);
-    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
-      console.log(response);
-    });
+    this.fetchprojects();
   },
 };
 </script>
 <template>
   <h1>Hello World</h1>
+  <hr />
+  <ProjectCard />
 </template>
 <style lang="scss"></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h2>ProjectCard</h2>
+</template>
+
+<style lang="scss" scoped></style>
