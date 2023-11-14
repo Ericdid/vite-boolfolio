af379ee44c8caeb73b935ce912110196f06ea329
Ericdid <ericdidom@gmail.com>
2023-11-14 17:29:58 +0100

    add.axios.call in project-detail.vue

--- src/pages/project-detail.vue
+++ src/pages/project-detail.vue
@@ -1,20 +1,33 @@
 <script>
 import ProjectCard from "../components/projects/ProjectCard.vue";
+import axios from "axios";
 
 export default {
   data() {
-    return {};
+    return {
+      api: {
+        baseUrl: "http://127.0.0.1:8000/api/",
+      },
+    };
   },
 
   components: {
     ProjectCard,
   },
+
+  created(url = this.api.baseUrl) {
+    console.log(url + "projects/" + this.$route.params.id);
+    axios.get(url + "projects/" + this.$route.params.id);
+  },
 };
 </script>
 
 <template>
-  <h1>Dettaglio Progetto</h1>
-  <ProjectCard />
+  <div class="container">
+    <h1 class="my-3">Dettaglio Progetto</h1>
+    <p>{{ this.$route.params.id }}</p>
+    <!-- <ProjectCard /> -->
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
